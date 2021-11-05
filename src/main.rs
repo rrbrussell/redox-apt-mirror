f56905eba81d9b88e82a481df655c63c6c95af06
Robert R. Russell <robert@rrbrussell.com>
2021-11-04 20:36:53 -0500

    First part of the data structures and example files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+//Copyright (C) 2021 Robert R. Russell
+//This program is free software; you can redistribute it and/or modify it under
+//the terms of the GNU General Public License as published by the Free Software
+//Foundation; version 2.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT
+//ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along with
+//this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+//Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#![deny(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+
+//! The redox-apt-mirror is a replacement for apt-mirror.
+
+use chrono::DateTime;
+use chrono::Utc;
+
+/// main does stuff
+///
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+}
+
+/// Which compression algorithm is used for an index.
+enum Compression {
+  NONE,
+  XZ,
+  GZIP,
+  BZIP2,
+  LZMA,
+}
+
+/// ReleaseData represents the control file that is used to define a repository
+struct ReleaseData {
+  description: Option<String>,
+  origin: Option<String>,
+  label: Option<String>,
+  version: Option<String>,
+  suite: String,
+  codename: String,
+  components: Vec<String>,
+  architectures: Vec<String>,
+  date: DateTime<Utc>,
+  valid_until: Option<DateTime<Utc>>,
+  files: Option<Vec<HashedFile>>,
+  not_automatic: Option<bool>,
+  but_automatic_upgrades: Option<bool>,
+  acquire_by_hash: Option<bool>,
+  signed_by: Option<Vec<String>>,
+}
+
+/// HashedFile represents the individual files linked to from the InRelease file.
+/// This struct does not seperate between Packages, Sources, Contents, or Indices.
+struct HashedFile {
+  name: String,
+  compression: Compression,
+  md5: Option<String>,
+  sha1: Option<String>,
+  sha256: Option<String>,
+  sha512: Option<String>,
 }
