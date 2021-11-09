64b1d0d4d7acc425055730fb88db0fc56dc8917c
Robert R. Russell <robert@rrbrussell.com>
2021-11-09 05:57:49 -0600

    The first couple of episodes.

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,19 @@
 
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
-
+#![allow(dead_code)]
+#![allow(unused_imports)]
 //! The redox-apt-mirror is a replacement for apt-mirror.
 
 use chrono::DateTime;
+use chrono::NaiveDateTime;
 use chrono::Utc;
 use std::error::Error;
 use std::fs::File;
-//use std::io;
-//use std::io::prelude::*;
-use std::io::{BufRead, BufReader};
-//use voca_rs::manipulate::trim;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Lines;
+use voca_rs::manipulate::trim;
 //use voca_rs::query;
 use voca_rs::Voca;
 
@@ -34,14 +36,83 @@ const FILENAME: &str = "Debian-InRelease";
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open(FILENAME)?;
     let file_reader = BufReader::new(file);
-    let mut release: ReleaseData;
-    for line in file_reader.lines().map(|l| l.unwrap()) {
-        if line.trim().starts_with("Date:") {
-            release.date = DateTime::parse_from_rfc2822(line).unwrap();
-            println!("{}", line);
+    let mut _data: ReleaseData = ReleaseData::new();
+    //let _date: DateTime<Utc>;
+    let mut lines = file_reader.lines().map(|l| l.unwrap());
+    loop {
+        let line = lines.next();
+        if line.is_some() {
+            let trimmed_line = trim(&line.unwrap(), " ");
+            if trimmed_line.contains("----BEGIN PGP SIGNED MESSAGE-----") {
+                // skip the Hash: line from an inline Ascii Armored signature
+                // and the blank line following it.
+                lines.next();
+                lines.next();
+                continue;
+            }
+            if trimmed_line.contains("-----BEGIN PGP SIGNATURE-----") {
+                // The ASCII Armored signature block is at the end of the file.
+                break;
+            }
+            // Process a Date tag.
+            if trimmed_line.starts_with("Date:") {
+                _data.date = parse_a_date(&trimmed_line);
+            }
+            // Process a suite tag
+            if trimmed_line.starts_with("Suite:") {
+                _data.suite = parse_a_suite(&trimmed_line);
+            }
+            // Process a codename
+            if trimmed_line.starts_with("Codename:") {
+                _data.codename = parse_a_codename(&trimmed_line);
+            }
+            if trimmed_line.starts_with("Components") {
+                _data.components = parse_the_components(&trimmed_line);
+            }
+        } else {
+            break;
         }
     }
+    println!("{:#?}", _data);
     return Ok(());
+}
+
+/// parses the components
+fn parse_the_components(input_line: &string) -> Vec<String> {
+    let index = input_line
+        .find(':')
+        .expect("A : went missing in parse_the_components");
+    let last_part = input_line[(index + 1)..].trim();
+    return last_part.split(' ');
+}
+
+/// parses a codename
+fn parse_a_codename(input_line: &String) -> String {
+    let index = input_line
+        .find(':')
+        .expect("A : went missing in parse_a_codename");
+    let last_part = input_line[(index + 1)..].trim();
+    return last_part.to_string();
+}
+
+/// parse a suite tag
+fn parse_a_suite(input_line: &String) -> String {
+    let index = input_line
+        .find(':')
+        .expect("A : went missing in parse_a_suite.");
+    let last_part = input_line[(index + 1)..].trim();
+    return last_part.to_string();
+}
+
+/// parses a date tag
+fn parse_a_date(input_line: &String) -> DateTime<Utc> {
+    let index = input_line.find(':').expect("could not find a :");
+    let last_part = input_line[(index + 1)..].trim();
+    let _date = DateTime::<Utc>::from_utc(
+        NaiveDateTime::parse_from_str(last_part, "%a, %d %b %Y %T %Z").unwrap(),
+        Utc,
+    );
+    return _date;
 }
 
 /// Which compression algorithm is used for an index.
@@ -54,13 +125,16 @@ enum Compression {
     LZMA,
 }
 
+/// The default is uncompressed.
+impl Default for Compression {
+    fn default() -> Compression {
+        return Compression::NONE;
+    }
+}
+
 /// ReleaseData represents the control file that is used to define a repository
 #[derive(Debug)]
 struct ReleaseData {
-    description: Option<String>,
-    origin: Option<String>,
-    label: Option<String>,
-    version: Option<String>,
     suite: String,
     codename: String,
     components: Vec<String>,
@@ -68,20 +142,50 @@ struct ReleaseData {
     date: DateTime<Utc>,
     valid_until: Option<DateTime<Utc>>,
     files: Option<Vec<HashedFile>>,
-    not_automatic: Option<bool>,
-    but_automatic_upgrades: Option<bool>,
     acquire_by_hash: Option<bool>,
-    signed_by: Option<Vec<String>>,
+}
+
+/// the stuff I manually implement for ReleaseData.
+impl ReleaseData {
+    /// Create a ReleaseData
+    fn new() -> ReleaseData {
+        return ReleaseData {
+            suite: String::new(),
+            codename: String::new(),
+            components: vec![String::new()],
+            architectures: vec![String::from("All"), String::from("Amd64")],
+            date: Utc::now(),
+            valid_until: None,
+            files: None,
+            acquire_by_hash: None,
+        };
+    }
 }
 
 /// HashedFile represents the individual files linked to from the InRelease file.
 /// This struct does not seperate between Packages, Sources, Contents, or Indices.
 #[derive(Debug)]
 struct HashedFile {
-    name: String,
     compression: Compression,
     md5: Option<String>,
+    name: String,
     sha1: Option<String>,
     sha256: Option<String>,
     sha512: Option<String>,
+    size: usize,
+}
+
+/// Implemented stuff for HashedFile.
+impl HashedFile {
+    fn new(name: String, compression: Compression, size: usize) -> HashedFile {
+        return HashedFile {
+            compression: compression,
+            md5: None,
+            name: name,
+            sha1: None,
+            sha256: None,
+            sha512: None,
+            size: size,
+        };
+    }
 }
