3866af9b3ee62624b37ec07f911adc390b4b4acb
Robert R. Russell <robert@rrbrussell.com>
2021-11-15 04:15:26 -0600

    Some code cleanup

--- src/main.rs
+++ src/main.rs
@@ -38,50 +38,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open(FILENAME)?;
     let file_reader = BufReader::new(file);
     let mut _data: ReleaseData = ReleaseData::new();
-    //let _date: DateTime<Utc>;
     let mut lines = file_reader.lines().map(|l| l.unwrap());
-    loop {
-        let line = lines.next();
-        if line.is_some() {
-            let trimmed_line = trim(&line.unwrap(), " ");
-            if trimmed_line.contains("----BEGIN PGP SIGNED MESSAGE-----") {
-                // skip the Hash: line from an inline Ascii Armored signature
-                // and the blank line following it.
-                lines.next();
-                lines.next();
-                continue;
-            }
-            if trimmed_line.contains("-----BEGIN PGP SIGNATURE-----") {
-                // The ASCII Armored signature block is at the end of the file.
-                break;
-            }
-            // Process a Date tag.
-            if trimmed_line.starts_with("Date:") {
-                _data.date = parse_a_date(&trimmed_line);
-            }
-            // Process a suite tag
-            if trimmed_line.starts_with("Suite:") {
-                _data.suite = parse_a_suite(&trimmed_line);
-            }
-            // Process a codename
-            if trimmed_line.starts_with("Codename:") {
-                _data.codename = parse_a_codename(&trimmed_line);
-            }
-            if trimmed_line.starts_with("Components:") {
-                _data.components = parse_the_components(&trimmed_line);
-            }
-            if trimmed_line.starts_with("Architectures:") {
-                _data.architectures = parse_the_architectures(&trimmed_line);
-            }
-        } else {
+    while let Some(line) = lines.next() {
+        let trimmed_line = trim(&line, " ");
+        if trimmed_line.contains("----BEGIN PGP SIGNED MESSAGE-----") {
+            // skip the Hash: line from an inline Ascii Armored signature
+            // and the blank line following it.
+            lines.next();
+            lines.next();
+            continue;
+        }
+        if trimmed_line.contains("-----BEGIN PGP SIGNATURE-----") {
+            // The ASCII Armored signature block is at the end of the file.
             break;
+        }
+        // Process a Date tag.
+        if trimmed_line.starts_with("Date:") {
+            _data.date = parse_a_date(&trimmed_line);
+        }
+        // Process a suite tag
+        if trimmed_line.starts_with("Suite:") {
+            _data.suite = parse_a_suite(&trimmed_line);
+        }
+        // Process a codename
+        if trimmed_line.starts_with("Codename:") {
+            _data.codename = parse_a_codename(&trimmed_line);
+        }
+        if trimmed_line.starts_with("Components:") {
+            _data.components = parse_the_components(&trimmed_line);
+        }
+        if trimmed_line.starts_with("Architectures:") {
+            _data.architectures = parse_the_architectures(&trimmed_line);
         }
     }
     println!("{:#?}", _data);
-    return Ok(());
+    Ok(())
 }
 
-fn parse_the_architectures(input_line: &String) -> Vec<String> {
+fn parse_the_architectures(input_line: &str) -> Vec<String> {
     let index = input_line
         .find(':')
         .expect("A : went missing in parse_the_architectures");
@@ -91,11 +85,11 @@ fn parse_the_architectures(input_line: &String) -> Vec<String> {
     for i in vecstr.iter() {
         v.push(String::from(*i));
     }
-    return v;
+    v
 }
 
 /// parses the components
-fn parse_the_components(input_line: &String) -> Vec<String> {
+fn parse_the_components(input_line: &str) -> Vec<String> {
     let index = input_line
         .find(':')
         .expect("A : went missing in parse_the_components");
@@ -105,36 +99,34 @@ fn parse_the_components(input_line: &String) -> Vec<String> {
     for i in vecstr.iter() {
         v.push(String::from(*i));
     }
-    return v;
+    v
 }
 
 /// parses a codename
-fn parse_a_codename(input_line: &String) -> String {
+fn parse_a_codename(input_line: &str) -> String {
     let index = input_line
         .find(':')
         .expect("A : went missing in parse_a_codename");
-    let last_part = input_line[(index + 1)..].trim();
-    return last_part.to_string();
+    input_line[(index + 1)..].trim().to_string()
 }
 
 /// parse a suite tag
-fn parse_a_suite(input_line: &String) -> String {
+fn parse_a_suite(input_line: &str) -> String {
     let index = input_line
         .find(':')
         .expect("A : went missing in parse_a_suite.");
     let last_part = input_line[(index + 1)..].trim();
-    return last_part.to_string();
+    last_part.to_string()
 }
 
 /// parses a date tag
-fn parse_a_date(input_line: &String) -> DateTime<Utc> {
+fn parse_a_date(input_line: &str) -> DateTime<Utc> {
     let index = input_line.find(':').expect("could not find a :");
     let last_part = input_line[(index + 1)..].trim();
-    let _date = DateTime::<Utc>::from_utc(
+    DateTime::<Utc>::from_utc(
         NaiveDateTime::parse_from_str(last_part, "%a, %d %b %Y %T %Z").unwrap(),
         Utc,
-    );
-    return _date;
+    )
 }
 
 /// Which compression algorithm is used for an index.
@@ -150,7 +142,7 @@ enum Compression {
 /// The default is uncompressed.
 impl Default for Compression {
     fn default() -> Compression {
-        return Compression::NONE;
+        Compression::NONE
     }
 }
 
@@ -170,7 +162,7 @@ struct ReleaseData {
 impl ReleaseData {
     /// Create a ReleaseData
     fn new() -> ReleaseData {
-        return ReleaseData {
+        ReleaseData {
             suite: String::new(),
             codename: String::new(),
             components: vec![String::new()],
@@ -178,7 +170,7 @@ impl ReleaseData {
             date: Utc::now(),
             files: None,
             acquire_by_hash: None,
-        };
+        }
     }
 }
 
@@ -198,14 +190,14 @@ struct HashedFile {
 /// Implemented stuff for HashedFile.
 impl HashedFile {
     fn new(name: String, compression: Compression, size: usize) -> HashedFile {
-        return HashedFile {
-            compression: compression,
+        HashedFile {
+            compression,
             md5: None,
-            name: name,
+            name,
             sha1: None,
             sha256: None,
             sha512: None,
-            size: size,
-        };
+            size,
+        }
     }
 }
